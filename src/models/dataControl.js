8ebba461881138b12be133e69b9aa0438e6a203b
unknown <cidluiz@gmail.com>
2019-11-25 22:07:11 -0300

    delete old unused src folder

--- src/models/dataControl.js
+++
@@ -1,8 +0,0 @@
-export class Bolsista{
-    constructor(cpf,nome,sexo, colaborador){
-        this.cpf = cpf;
-        this.nome = nome;
-        this.sexo = sexo;
-        this.colaborador = colaborador;
-    }
-}
--- src/views/uiControl.js
+++
@@ -1,8 +0,0 @@
-export const dom = {
-    sendData: document.querySelector('.sendForm'),
-    cpfInput: document.getElementById('cpf'),
-    nomeInput: document.getElementById('nome'),
-    sexoInput: document.querySelector('.sexo'),
-    colabInput: document.querySelector('.clbr'),
-    saveInput: document.querySelector('.save_button'),
-};
