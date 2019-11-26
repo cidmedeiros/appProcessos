dcbafe526fa8ae684ead2fe83b95bc6bd157a7cc
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2019-11-26 19:01:54 -0300

    make programa schema

---
+++ models/programa.js
@@ -0,0 +1,18 @@
+mongoose = require('mongoose');
+
+const programaSchema = new mongoose.Schema({
+	nome: String,
+	sigla: String,
+	coordNacional: [
+		{
+			ies: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Ies'
+			},
+			inicio: Date,
+			fim: Date
+		}
+	]
+});
+
+module.exports = mongoose.model('Programa', programaSchema);
