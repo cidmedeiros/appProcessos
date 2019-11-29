40c38c15d1577cc1084fc66d05ffe9060733ae52
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2019-11-29 16:14:44 -0300

    Add pagsJson( )

--- assets/tojson.py
+++ assets/tojson.py
@@ -66,3 +66,42 @@ def match_name(name, list_names, min_score=0):
             
     return (max_name, max_score)
 
+def pagsJson(min_score=0):
+    
+    """
+    """
+    ies = pd.read_csv('dados\ies.csv', sep=';', encoding='ISO-8859-1')
+    ies = list(ies.nome_entidade)
+    pags = pd.read_excel('dados\proeb_2011_2012_profmat.xlsx', encoding='ISO-8859-1')
+    
+    nac_ies = list(pags.ies.drop_duplicates())
+    nac_ies = [ies.upper() for ies in nac_ies]
+    series_nacional = []
+    
+    for nome in nac_ies:
+        ans = match_name(nome, ies, min_score)
+        series = pd.Series()
+        series['ies'] = nome
+        series['match_name'] = ans[0]
+        series['match_score'] = ans[1]
+        series_nacional.append(series)
+        
+    ies_nacional = pd.DataFrame()
+    ies_nacional = ies_nacional.append(series_nacional)
+    
+    local_ies = list(pags.iesLocal.drop_duplicates())
+    local_ies = [ies.upper() for ies in local_ies]
+    series_local = []
+    
+    for nome in local_ies:
+        ans = match_name(nome, ies, min_score=75)
+        series = pd.Series()
+        series['iesLocal'] = nome
+        series['match_name'] = ans[0]
+        series['match_score'] = ans[1]
+        series_local.append(series)
+        
+    ies_local = pd.DataFrame()
+    ies_local = ies_local.append(series_local)
+        
+    return ies_nacional, ies_local
