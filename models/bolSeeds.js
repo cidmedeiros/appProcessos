2324baa796def39240806cfb58cdd4603463c0c2
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2019-12-05 17:16:01 -0300

    Add preProcBol( ) using async/await

--- models/bolSeeds.js
+++ models/bolSeeds.js
@@ -20,56 +20,33 @@ const cleanBols = function() {
     });
 };
 
-const associateBols = function(bolsista){
-    ans = [];
-    for(let i = 0; i <= (bolsista.pags.length-1); i++){
-        Programa.findOne({'nome': bolsista.pags[i]['programa']}, (err, foundPro) => {
-                if(err){
-                    console.log(`Error finding programa ${bolsista.pags[i]['programa']}, ${err}`);
-                } else {
-                    bolsista.pags[i]['programa'] = foundPro;
-                }
-        })
-        .exec()
-        .then(Ies.findOne({'sigla':bolsista.pags[i]['iesLocal']}, (err, foundIes) => {
-            if(err){
-                console.log(`Error finding Ies ${bolsista.pags[i]['iesLocal']}, ${err}`);
-            } else {
-                bolsista.pags[i]['iesLocal'] = foundIes;
-                let ref = bolsista.pags[i]['dataRef'].split('/');
-                bolsista.pags[i]['dataRef'] = new Date(+ref[2], ref[1] - 1, +ref[0]);
-                let dPag = bolsista.pags[i]['dataPag'].split('/');
-                bolsista.pags[i]['dataPag'] = new Date(+dPag[2], dPag[1] - 1, +dPag[0]);
-            }
-            })
-        )
-        
-    };
-    console.log(`-------------Printing associateBols----------------------`);
-    ans.push(bolsista);
-    console.log(ans[0]['pags'][0]);
-    return ans;
-}
+const preProcBol = async (bolsista) => {
+    let indexes = [];
 
-const addBols = function(bolsData) {
-    const bolsistasData = preProcessBols(bolsData);
-    try{
-        Bolsista.insertMany(bolsistasData, (err, data) =>{
+    for (let i = 0; i < bolsista.pags.length; i++){
+        indexes.push(i);
+    }
+
+    for(i of indexes){
+        bolsista.pags[i].programa = await Programa.findOne({'nome':bolsista.pags[i].programa},(err, foundOne) => {
             if(err){
-                console.log(`Error 1 trying to save bolsistas ${err}`);
-            } else{
-                console.log('Bolsistas saved!');
+                console.log(err)
+            } else {
+            return foundOne
             }
         });
-    } catch(error){
-        console.log(`Error 2 trying to save bolsistas ${error}`);
+
+         bolsista.pags[i].iesLocal = await Ies.findOne({'sigla': bolsista.pags[i].iesLocal}, (err, foundOne) => {
+            if(err){
+                console.log(err)
+            } else {
+                return foundOne
+            }
+        });
+        let ref = bolsista.pags[i].dataRef.split('/');
+        bolsista.pags[i].dataRef = new Date(+ref[2], ref[1] - 1, +ref[0]);
+        let dPag = bolsista.pags[i].dataPag.split('/');
+        bolsista.pags[i].dataPag = new Date(+dPag[2], dPag[1] - 1, +dPag[0]);
     }
+    return bolsista
 };
-
-const seedBols = function(){
-    cleanBols();
-    addBols();
-};
-
-associateBols(bolsData[0]);
-
