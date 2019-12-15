a3d430b734f14cd227cf6360f8f4bd88ed529d13
unknown <cidluiz@gmail.com>
2019-12-15 12:14:03 -0300

    improve seeding bolsistas monitoring

--- models/bolSeeds.js
+++ models/bolSeeds.js
@@ -1,6 +1,6 @@
 //Set DataBase
 mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/testDB', {'useNewUrlParser': true, 'useUnifiedTopology':false});
+mongoose.connect('mongodb://localhost:27017/testDB', {'useNewUrlParser': true, 'useUnifiedTopology':true});
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
@@ -73,13 +73,14 @@ const addBol = async (sPath) => {
     try{
         for(bolsista of bolsData){
             refactored.push(await preProcBol(bolsista));
-            console.log('Saving Bolsistas...')
+            console.log('Adding Bolsistas...');
         }
     } catch(error){
         console.log(`Populating refactored failed: ${error}`)
     }
 
     if(refactored.length > 0){
+        console.log('Saving Bolsistas...');
         Bolsista.insertMany(refactored, (err, data) => {
             if(err){
                 console.log(`InserMany error handling -> ${err}`);
