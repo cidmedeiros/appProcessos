d1f56c3ac38d708e4c6c90dc441620523bd58de3
unknown <cidluiz@gmail.com>
2020-01-05 13:35:10 -0300

    add button plus and card style to declara

--- assets/scripts/tools.js
+++ assets/scripts/tools.js
@@ -3,7 +3,7 @@ function getLocalIp() {
 
     for(let addresses of Object.values(os.networkInterfaces())) {
         for(let add of addresses) {
-            if(add.address.startsWith('172.19.')) {
+            if(add.address.startsWith('192.')) {
                 return add.address;
             }
         }
