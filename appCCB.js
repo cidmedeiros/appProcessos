5e16277a963a552e72cea1a0b722a72b1b4ad61c
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2020-01-07 11:29:45 -0300

    Add new route to add declaracao

--- appCCB.js
+++ appCCB.js
@@ -172,20 +172,44 @@ app.put('/editarcompromisso/:cpf', async (req, res) =>{
 
 					'statusCurso':{status:req.body.bolsista.status, data: new Date(req.body.bolsista.dataConcl), user:'teste'},
 					'certConclusao':{ies:iesUpdate,regular:req.body.bolsista.regCert,obsv:req.body.bolsista.obsvCert,user:'teste',data: new Date()},
-					'declaracao':{municipioEscola:{nome:req.body.bolsista.munDecl, uf:req.body.bolsista.ufDecl}, permanencia:parseInt(req.body.bolsista.perm, 10), regular:req.body.bolsista.regDecl,obsv:req.body.bolsista.obsvDecl, user:'teste', data: new Date()},
 					'analiseCompromisso':{regular:req.body.bolsista.resAna,obsv:req.body.bolsista.obsvAna, user:'teste', data: new Date()}
 				}
 			},
 			(err, upObejct) =>{
 				if(err){
 					console.log('------------------------------------');
-					console.log(`Second Promise Internal error updating ${err}`);
+					console.log(`First Promise Internal error updating ${err}`);
+					console.log('------------------------------------');
+				} else{
+					res.redirect(`/paginadobolsista/${upObejct._id}`);
+				}
+			});
+	} catch(error) {
+		console.log('------------------------------------');
+		console.log(`Exit Promise External error updating ${error}`);
+		console.log('------------------------------------');
+	}
+});
+
+app.put('/adddeclaracao/:cpf',  async (req, res) =>{
+	//Push Declaracao
+	try{
+		Bolsista.findOneAndUpdate({cpf:req.params.cpf},
+			{
+				'$push':{
+					'declaracao':{municipioEscola:{nome:req.body.bolsista.munDecl, uf:req.body.bolsista.ufDecl}, permanencia:parseInt(req.body.bolsista.perm, 10), regular:req.body.bolsista.regDecl, obsv:req.body.bolsista.obsvDecl, user:'teste', data: new Date()},
+				}
+			},
+			(err, upObejct) =>{
+				if(err){
+					console.log('------------------------------------');
+					console.log(`First Promise Internal error updating ${err}`);
 					console.log('------------------------------------');
 				} else{
 					Bolsista.findOne({cpf:req.params.cpf}, async (err, foundBol) => {
 						if(err){
 							console.log('------------------------------------');
-							console.log(`Third Promise Internal error finding bolsista ${err}`)
+							console.log(`Second Promise Internal error finding bolsista ${err}`)
 							console.log('------------------------------------');
 						} else{
 							console.log(foundBol.declaracao)
@@ -194,7 +218,7 @@ app.put('/editarcompromisso/:cpf', async (req, res) =>{
 							Bolsista.findOneAndUpdate({cpf:req.params.cpf}, {'permanenciaTotal':newPerm}, (err, upObejctInt) =>{
 								if(err){
 									console.log('------------------------------------');
-									console.log(`Forth Promise Internal error updating after Calculating ${newPerm} ${err}`);
+									console.log(`Third Promise Internal error updating after Calculating ${newPerm} ${err}`);
 									console.log('------------------------------------');
 								} else {
 									res.redirect(`/paginadobolsista/${upObejctInt._id}`);
@@ -210,7 +234,7 @@ app.put('/editarcompromisso/:cpf', async (req, res) =>{
 		console.log(`Exit Promise External error updating ${error}`);
 		console.log('------------------------------------');
 	}
-});	
+});
 
 	//Routes order matters! This should always be the last route!!
 app.get('*', async (req, res) =>{
