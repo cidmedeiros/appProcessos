b5088a9efb4a3d06355e4f5213997379297ca1c8
Cid Medeiros <cidluiz@gmail.com>
2020-07-02 15:31:14 -0300

    finish query

--- assets/scripts/queries/processosByFilter.js
+++ assets/scripts/queries/processosByFilter.js
@@ -6,6 +6,7 @@ mongoose.set('useCreateIndex', true);
 var util = require('util');
 var tools = require('../tools');
 var Bolsista = require('../../../models/bolsistas');
+var User = require('../../../models/user');
 
 Bolsista.aggregate([
     {$match:
@@ -17,22 +18,40 @@ Bolsista.aggregate([
             curso: {$arrayElemAt: ['$statusCurso', -1]},
             lastAnalise: {$arrayElemAt:['$analiseCompromisso', -1]},
             lastTermo: {$arrayElemAt:['$termo', -1]},
+            lastClbr: {$arrayElemAt: ['$clbr', -1]}
         }
     },
     {$match:
         {
-            $and: [{'curso.status': {$in : ['Concluído', 'Desligado', 'Concluído (Após Prazo Regular)']}}, {permanencia: {$in: ['Sem Permanência']}}]
+            $and: [{'curso.status': {$in : ['Concluído', 'Concluído (Após Prazo Regular)']}}, {permanencia: {$in: ['Sem Permanência']}}]
         }
     },
     {$set:
-        {conclusao: '$curso.status'}
+        {conclusao: '$curso.status', colaborador:'$lastClbr.user'}
     },
     {$project:
-        {sei:1, cpf:1, nome:1, conclusao:1, permanencia:1}
+        {_id: 0, sei:1, cpf:1, nome:1, conclusao:1, permanencia:1, colaborador:1}
     },
 ], (err, ans) => {
     if(!err){
-        tools.saveToCsv('processosByFilter', ans);
+        User.populate(ans, {path:'colaborador'}, (err, ansPop) => {
+            if(!err){
+                var data = [];
+                for(item of ansPop){
+                    newObj ={};
+                    newObj.cpf = item.cpf;
+                    newObj.sei = item.sei;
+                    newObj.nome = item.nome;
+                    newObj.conclusao = item.conclusao;
+                    newObj.permanencia = item.permanencia;
+                    newObj.colaborador = item.colaborador.fullname;
+                    data.push(newObj);
+                }
+                tools.saveToCsv('processosByFilter', data);
+            } else{
+                console.log(err);
+            }
+        });
     } else{
         console.log(err);
     }
